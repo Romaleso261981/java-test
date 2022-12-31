3c214626d85fc2b9a3323e36c480a2cef1e10b35
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-12-31 19:21:40 +0300

    added any function evenNumber

--- src/Human/heloworld.java
+++ src/Human/heloworld.java
@@ -1,18 +1,26 @@
 package Human;
 
 class Human {
-    int age;
+    int[] marks = new int[] {11, 3, 5, 12, 8, 4, 7, 2, 15, 10};
+    int age = 10;
 
     public static void main(String[] args) {
         Human human = new Human();
-        human.buyBread();
-        human.buyMilk();
         human.accessToAlcohol();
-        System.out.println("helloWorld");
+        human.evenNumber();
+        human.buyBread();
     }
 
-    void buyMilk() {
-        System.out.println("buyMilk");
+    void evenNumber() {
+        for (int i = 0; i < marks.length; i += 1) {
+            System.out.println(marks[i]);
+            if(marks[i] % 2 == 0){
+                System.out.println("the number is even");
+        }else{
+                System.out.println("the number is not even");
+            }
+
+    }
     }
 
     void buyBread() {
@@ -20,7 +28,6 @@ class Human {
     }
 
     void accessToAlcohol() {
-        int age = 19;
         if (age > 18) {
             System.out.println("access is open");
         } else {
---
+++ src/evenNumber/evenNumber.java
@@ -0,0 +1,37 @@
+//package Human;
+//
+//class Human2 {
+//    int[] marks = new int[] {11, 3, 5, 12, 8, 4, 7, 2, 15, 10};
+//    int age;
+//
+//    public static void main(String[] args) {
+//        Human human = new Human();
+//        human.evenNumber();
+//        System.out.println("helloWorld");
+//    }
+//
+//    void evenNumber() {
+//        for (int i = 0; i < marks.length; i += 1) {
+//            System.out.println(marks[i]);
+//            if(marks[i] % 2 == 0){
+//                System.out.println("the number is even");
+//            }else{
+//                System.out.println("the number is not even");
+//            }
+//
+//        }
+//    }
+//
+//    void buyBread() {
+//        System.out.println("buyBread");
+//    }
+//
+//    void accessToAlcohol() {
+//        int age = 19;
+//        if (age > 18) {
+//            System.out.println("access is open");
+//        } else {
+//            System.out.println("access is not open");
+//        }
+//    }
+//}
