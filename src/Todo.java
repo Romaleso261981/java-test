4319f57e21a147430fdcfd3bd6e3c365e84922ea
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2023-01-20 23:11:18 +0300

    added any function evenNumber

--- src/Todo.java
+++ src/Todo.java
@@ -1,30 +1,35 @@
 
 import java.util.Scanner;
-
+import java.util.Arrays;
 
 public class Todo {
 
     public static void main(String[] args) {
         Number[] age = {12, 4, 5, 2, 5};
         System.out.println(Arrays.toString(age));
+        Welcome();
     }
 
     public static void Welcome() {
 
         System.out.println("Welcome to your Todo Manager!\n");
-        System.out.println("Please Enter your Name: ");
-        Scanner in = new Scanner(System.in);
-        String name = in.next();
-        System.out.println(name);
-        if (name == "Roma") {
-            System.out.println("Hello " + name);
+        System.out.println("Please write any number from 1 to 10 ");
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextInt()) {
+            int number = sc.nextInt();
+            if (number > 1 && number < 10) {
+                System.out.println("your number is out of range");
+            } else {
+                System.out.println("your number " + number + " is within the given limits");
+            }
+        } else {
+            System.out.println("Извините, но это явно не число. Перезапустите программу и попробуйте снова!");
         }
-        System.out.println("Name not correct");
+
 
     }
 
     ;
-
 
 
 }
