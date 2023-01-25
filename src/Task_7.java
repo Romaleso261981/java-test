acb9580355d32662311b7fa5432cae8278207abc
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2023-01-25 12:08:42 +0300

    added function ollSumNum

--- src/Task_7.java
+++ src/Task_7.java
@@ -5,6 +5,7 @@ public class Task_7 {
     public static void main(String args[]) {
         printNum(10);
         sumOdd(25);
+        ollSumNum(10);
     }
 
     public static void printNum(int N) {
@@ -14,20 +15,33 @@ public class Task_7 {
             System.out.println(i);
         }
 
-    }public static void sumOdd(int N) {
-        int i, sum = 0;
+    }
+
+    public static void ollSumNum(int N) {
+        int sum = 0;
+
+        for (int i = 0; i <= N; i++) {
+            sum += i;
+        }
+        System.out.println("Sum of all numbers between 0 to "
+                + N + " = " + sum);
+    }
+
+
+    public static void sumOdd(int N) {
+        int sum = 0;
 
 //        Scanner in = new Scanner(System.in);
 //        System.out.println("Enter a number");
 //        N = in.nextInt();
 
-        for (i = 0; i <= N; i++) {
+        for (int i = 0; i <= N; i++) {
             if ((i % 2) == 1) {
                 sum += i;
             }
         }
 
-        System.out.print("Sum of all odd numbers between 0 to "
+        System.out.println("Sum of all odd numbers between 0 to "
                 + N + " = " + sum);
     }
 }
