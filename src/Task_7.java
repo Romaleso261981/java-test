5e8fa7faa727834f7951c70a16c1f77d4da6320d
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2023-01-25 12:21:02 +0300

    added function descendingArr

--- src/Task_7.java
+++ src/Task_7.java
@@ -3,9 +3,11 @@ import java.util.*;
 
 public class Task_7 {
     public static void main(String args[]) {
-        printNum(10);
-        sumOdd(25);
-        ollSumNum(10);
+//        printNum(10);
+//        sumOdd(25);
+//        ollSumNum(10);
+        ascendingArr();
+//        descendingArr();
     }
 
     public static void printNum(int N) {
@@ -43,5 +45,54 @@ public class Task_7 {
 
         System.out.println("Sum of all odd numbers between 0 to "
                 + N + " = " + sum);
+    }
+
+    public static void ascendingArr() {
+        int [] arr = new int [] {5, 2, 8, 7, 1};
+        int temp = 0;
+
+        System.out.println("Elements of original array: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[i] > arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println("Elements of array sorted in ascending order: ");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void descendingArr() {
+        int [] arr = new int [] {5, 2, 8, 7, 1};
+        int temp = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[i] < arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println("Elements of array sorted in ascending order: ");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
     }
 }
