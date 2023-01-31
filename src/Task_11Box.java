2686a34793728533fc074fc73dc55800b8d51af5
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2023-01-31 18:57:31 +0300

    create class Box

---
+++ src/Task_11Box.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Task_11Box {
+    String value = "Hi";
+
+    public static void main(String[] args) {
+
+    }
+    public void changeValue(String s) {
+        value = s;
+    }
+
+
+    public static void log(String s) {
+        System.out.println(s);
+    }
+}
