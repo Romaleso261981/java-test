4bfdbde6b7f17779a2ead43ba1ce159e1886fad0
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2023-01-31 18:39:07 +0300

    Update Task_10.java

--- src/Task_10.java
+++ src/Task_10.java
@@ -11,6 +11,9 @@ public class Task_10 {
         while (k) {
             log("enter the task number");
             log("check whether the given term ends with the specified substring ---- write 1");
+            log("String objects contain the same data ---- write 2");
+            log("compares the given string with another string, ignoring the case ---- write 3");
+            log("determines whether the period begins with a sub-period ---- write 4");
             Scanner input = new Scanner(System.in);
             int num = input.nextInt();
             switch (num) {
@@ -18,13 +21,13 @@ public class Task_10 {
                     endsWithTheSpecifiedSubstring();
                     break;
                 case 2:
-                    sum();
+                    stringObjectsContainTheSameData();
                     break;
                 case 3:
-                    recursions();
+                    comparesTheGivenStringWithAnotherStringIgnoringTheCase();
                     break;
                 case 4:
-                    starOutput();
+                    determinesWhetherPeriodBeginsWithSubPeriod();
                     break;
                 case 5:
                     log("5 - Recursive method of exponentiation");
@@ -45,14 +48,28 @@ public class Task_10 {
         System.out.println(bool1);
     }
 
-    public static void sum() {
+    public static void stringObjectsContainTheSameData() {
+        String s = "Recursive method";
+        String suffix = "tid";
+        boolean bool1 = s.endsWith(suffix);
+
+        System.out.println(bool1);
     }
 
-    public static void recursions() {
+    public static void comparesTheGivenStringWithAnotherStringIgnoringTheCase() {
+        String s = "Stephen Edwin King";
+        String s2 = "Walter Winchell";
+        boolean bool1 = s.equalsIgnoreCase(s2);
 
+        System.out.println(bool1);
     }
 
-    public static void starOutput() {
+    public static void determinesWhetherPeriodBeginsWithSubPeriod() {
+        String s = "Stephen Edwin King";
+        String subPeriod = "Ste";
+
+        boolean bool1 = s.startsWith(subPeriod);
+        System.out.println(bool1);
     }
 
     public static void log(String s) {
