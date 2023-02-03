8b26de4e646e18532fe7afc9a67597c9dfa7a305
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2023-02-03 13:16:08 +0300

    Update Task_11class.java

--- src/Task_11class.java
+++ src/Task_11class.java
@@ -1,4 +1,24 @@
-public class Task_11class {
-
-    public static void main(String[] args) {}
+public class Main {
+    public static void main(String[] args) {
+        Box myObj = new Box();
+        myObj.value = "John";
+        System.out.println(myObj.value);
+        Box myObj2 = new Box();
+        myObj2.value = "Steven";
+        System.out.println(myObj2.value);
+    }
 }
+
+
+class Box {
+    String value = "";
+
+    public String getName() {
+        return value;
+    }
+
+    public void setName(String newName) {
+        this.value = newName;
+    }
+}
+
